8cdb1d66022677a80c15660b43aa04a4885fd8f1
STOMACO Studio <69104463+STOMACOstudio@users.noreply.github.com>
2025-06-27 18:51:14 +0200

    Fix repeated AI combat damage

--- Assets/Scripts/TurnSystem.cs
+++ Assets/Scripts/TurnSystem.cs
@@ -922,13 +922,8 @@ public class TurnSystem : MonoBehaviour
 
                 case TurnPhase.Damage:
                     Debug.Log("→ Resolving combat damage.");
-                    if (damageCoroutine != null)
-                    {
-                        StopCoroutine(damageCoroutine);
-                        damageCoroutine = null;
-                    }
-
-                    damageCoroutine = StartCoroutine(WaitToShowCombatDamage());
+                    if (damageCoroutine == null)
+                        damageCoroutine = StartCoroutine(WaitToShowCombatDamage());
                     break;
 
                 case TurnPhase.EndTurn:
