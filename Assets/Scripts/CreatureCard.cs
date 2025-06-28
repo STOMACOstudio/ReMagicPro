8ba50fadc2a4403fd90e90b6c7b7684e067251f5
STOMACO Studio <69104463+STOMACOstudio@users.noreply.github.com>
2025-06-28 13:07:29 +0200

    Add Gorilla Chief card and monkey buff system

--- Assets/Scripts/CreatureCard.cs
+++ Assets/Scripts/CreatureCard.cs
@@ -12,11 +12,27 @@ public class CreatureCard : Card
     public int minusOneCounters = 0;
     public int tempPowerBonus = 0;
     public int tempToughnessBonus = 0;
+    public int monkeyBuffSources = 0; // number of Gorilla Chief effects
 
     public void RecalculateStats()
     {
-        power = basePower + plusOneCounters - minusOneCounters + tempPowerBonus;
-        toughness = baseToughness + plusOneCounters - minusOneCounters + tempToughnessBonus;
+        power = basePower + plusOneCounters - minusOneCounters + tempPowerBonus + monkeyBuffSources;
+        toughness = baseToughness + plusOneCounters - minusOneCounters + tempToughnessBonus + monkeyBuffSources;
+    }
+
+    public void AddMonkeyBuff()
+    {
+        monkeyBuffSources++;
+        RecalculateStats();
+    }
+
+    public void RemoveMonkeyBuff()
+    {
+        if (monkeyBuffSources > 0)
+        {
+            monkeyBuffSources--;
+            RecalculateStats();
+        }
     }
 
     public void AddPlusOneCounter()
