dad6971f7bee440848f5f15866a1e6c71d9e707d
STOMACO Studio <69104463+STOMACOstudio@users.noreply.github.com>
2025-07-25 22:38:57 +0200

    Unlock cursor when returning from IsoWorld

--- Assets/Scripts/ReturnToColorSelection.cs
+++ Assets/Scripts/ReturnToColorSelection.cs
@@ -5,6 +5,10 @@ public class ReturnToColorSelection : Interactable
 {
     public override void Interact()
     {
+        // Unlock and show the cursor before leaving the first person scene
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
         SceneManager.LoadScene("ColorSelectScene");
     }
 }
